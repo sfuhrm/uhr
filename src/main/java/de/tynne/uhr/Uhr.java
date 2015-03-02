9ce3cb136f5bc39c010ea32b3496850f673c0b0b
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2015-03-02 13:03:08 +0100

    Refactored out the stroke width

--- src/main/java/de/tynne/uhr/Uhr.java
+++ src/main/java/de/tynne/uhr/Uhr.java
@@ -51,6 +51,12 @@ public class Uhr extends JPanel {
 	private final static Color MINUTES_ARM_COLOR = new Color(0f, 0f , 0f, 0.5f);
 	private final static Color HOURS_ARM_COLOR = new Color(1f, 0f , 0f, 0.5f);
 	
+	private final float SECONDS_STROKE = 2.1f;
+	private final float MINUTES_STROKE = 4f;
+	private final float HOURS_STROKE = 4f;
+	private final float HOUR_TICKS_STROKE = 4f;
+	private final float MINUTE_TICKS_STROKE = 1f;
+	
 	/**
 	 * Draws the clock.
 	 * @param g2d the graphics context
@@ -68,29 +74,29 @@ public class Uhr extends JPanel {
 		
 		// minute/second ticks
 		g2d.setColor(MINUTE_TICKS_COLOR);
-		g2d.setStroke(new BasicStroke(1f));
+		g2d.setStroke(new BasicStroke(MINUTE_TICKS_STROKE));
 		for (int i=0; i < 60; i++) {
 			drawCircleLine(g2d, r, cx, cy, i, 60, 1f-0.05f, 1f);
 		}		
 		
 		// hour ticks
 		g2d.setColor(HOUR_TICKS_COLOR);
-		g2d.setStroke(new BasicStroke(4f));
+		g2d.setStroke(new BasicStroke(HOUR_TICKS_STROKE));
 		for (int i=0; i < 12; i++) {
 			drawCircleLine(g2d, r, cx, cy, i, 12, 1f-0.2f, 1f);
 		}
 		
 		// seconds
 		g2d.setColor(SECONDS_ARM_COLOR);
-		g2d.setStroke(new BasicStroke(2.1f));
+		g2d.setStroke(new BasicStroke(SECONDS_STROKE));
 		drawCircleLine(g2d, r, cx, cy, seconds, 60, 0., 1f);
 		// minutes
 		g2d.setColor(MINUTES_ARM_COLOR);
-		g2d.setStroke(new BasicStroke(4));
+		g2d.setStroke(new BasicStroke(MINUTES_STROKE));
 		drawCircleLine(g2d, r, cx, cy, minutes, 60, 0., 0.9);
 		// hours
 		g2d.setColor(HOURS_ARM_COLOR);
-		g2d.setStroke(new BasicStroke(4));
+		g2d.setStroke(new BasicStroke(HOURS_STROKE));
 		drawCircleLine(g2d, r, cx, cy, hours + minutes  / 60., 12, 0., 0.8);
 	}
 	
